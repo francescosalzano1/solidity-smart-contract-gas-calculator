1d2db5ab08ef2465e4fb8cd20dd392ab3f1c2f26
francesco.salzano <francesco.salzanosf@gmail.com>
2023-10-01 21:55:36 +0200

    truffle test to compute gas implemented

--- test/simpleStorage.js
+++ test/simpleStorage.js
@@ -1,23 +1,14 @@
-const SimpleStorage = artifacts.require("SimpleStorage") 
+const SimpleStorage = artifacts.require("SimpleStorage")
 
 contract('SimpleStorage', (accounts) => {
-	const [bob, alice] = accounts
-	it("should verify bob and alice's favorite numbers default to 0", async () => {
-		const ssContract = await SimpleStorage.deployed()
-		const bobNum = await ssContract.favoriteNumbers.call(bob)
-		assert.equal(bobNum, 0,
-			"bob's default value was non-zero")
-		
-		const aliceNum = await ssContract.favoriteNumbers.call(alice)
-		assert.equal(aliceNum, 0,
-			"alice's default value was non-zero")
-	})
+    const [bob, alice] = accounts
+    it("should verify bob and alice's favorite numbers default to 0", async () => {
+        const ssContract = await SimpleStorage.deployed()
+        const result = await ssContract.setFavorite(1)
+        console.log("risultato: ")
+        console.log(result.receipt.gasUsed.toString())
 
-	it("should set bob's favorite number to 33", async () => {
-		const ssContract = await SimpleStorage.deployed()
-		ssContract.setFavorite(33, {from: bob})
-		const newBobNum = await ssContract.favoriteNumbers.call(bob)
-		assert.equal(newBobNum, 30,
-			"bob's new value was not 33")
-	})
+    })
+
+
 })
